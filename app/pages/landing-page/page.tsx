227845ea9139c6f6767c6e2b01df1bbf099eb438
bignontohounkpo <bpriscatohounkpo@gmail.com>
2025-08-27 08:46:59 +0200

    feat: add banniere section

--- app/pages/landing-page/page.tsx
+++ app/pages/landing-page/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './hero-section/page'
+import HeritageSection from './bannière/page'
 
 const LandingPage = () => {
   return (
     <div>
         <HeroSection />
+        <HeritageSection />
     </div>
   )
 }
