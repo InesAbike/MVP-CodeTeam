5ff8823ab377a6be0abe8f7be77f47d9f6965052
Iovann <iovannatcho@gmail.com>
2025-09-14 18:22:33 +0100

    style:improve landing page  style  and link search page

--- app/components/landing-page/DecouverteSection.tsx
+++ app/components/landing-page/DecouverteSection.tsx
@@ -1,29 +1,30 @@
 import Image from "next/image";
 
-const HeritageSection=() => {
+const HeritageSection = () => {
   return (
     <div className="py-10">
-    <div className="relative w-full h-[500px] flex items-center justify-center ">
-      {/* Image de fond */}
-      <Image
-        src="/images/place-amazone.svg" 
-        alt="Statue Amazone - Bénin"
-        fill
-        className="object-cover brightness-60"
-        priority
-      />
+      <div className="relative w-full h-[600px] flex items-center justify-center ">
+        {/* Image de fond */}
+        <Image
+          src="/images/place-amazone.svg"
+          alt="Statue Amazone - Bénin"
+          fill
+          className="object-cover brightness-60"
+          priority
+        />
 
-      {/* Overlay texte */}
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6">
-        <h2 className="text-5xl lg:text-6xl  font-bold text-white drop-shadow-lg  leading-tight">
-          Découvrez l’histoire et l’héritage du Bénin
-        </h2>
-        <p className="mt-4 text-xl text-gray-100 max-w-2xl drop-shadow-md">
-          Autour de chaque monument,  vivez aussi l’art et la créativité de nos artisans locaux.
-        </p>
+        {/* Overlay texte */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-3 md:px-6">
+          <h2 className="text-3xl md:text-5xl xl:text-6xl  font-bold text-white drop-shadow-lg  leading-tight">
+            Découvrez l’histoire et l’héritage du Bénin
+          </h2>
+          <p className="mt-4 text-base sm:text-xl text-gray-100 max-w-2xl drop-shadow-md">
+            Autour de chaque monument, vivez aussi l’art et la créativité de nos
+            artisans locaux.
+          </p>
+        </div>
       </div>
     </div>
-    </div>
   );
-}
-export default HeritageSection 
+};
+export default HeritageSection;
--- app/touristicsite/[id]/page.tsx
+++ app/touristicsite/[id]/page.tsx
@@ -11,14 +11,15 @@ import { useParams } from "next/navigation";
 import { getOpeningStatus } from "@/lib/utils";
 import { RiShoppingBag3Line } from "react-icons/ri";
 
-
-
 export default function DetailPage() {
   const params = useParams();
   const id = params.id as string;
 
   const site = useGetTouristicSiteById(id as Id<"touristicSites">);
-  const nearbyArtisansWithDetails = useGetNearbyArtisanShopsWithDetailsBySiteId(id as Id<"touristicSites">, 6);
+  const nearbyArtisansWithDetails = useGetNearbyArtisanShopsWithDetailsBySiteId(
+    id as Id<"touristicSites">,
+    6,
+  );
 
   if (site === undefined) {
     return (
@@ -29,7 +30,11 @@ export default function DetailPage() {
   }
 
   if (!site) {
-    return <p className="p-6 h-screen flex items-center justify-center">Site introuvable</p>;
+    return (
+      <p className="p-6 h-screen flex items-center justify-center">
+        Site introuvable
+      </p>
+    );
   }
 
   const transformedSite = {
@@ -43,38 +48,45 @@ export default function DetailPage() {
     price: site.entranceFee ? `${site.entranceFee} FCFA` : "Gratuit",
     accessibility: "",
     image: site.images[0] || "/images/hero-benin.png",
-    artisans: [] 
+    artisans: [],
   };
 
   return (
-    <div className="space-y-6 bg-[#FFFBEB] min-h-screen">
+    <div className="space-y-6 bg-gray-50 min-h-screen">
       {/* Composant Site */}
       <TouristSiteCard {...transformedSite} />
 
-       {/* Composant Artisans */}
-       <div className="max-w-3xl mx-auto pb-5 ">
-         <h3 className="text-base md:text-lg font-semibold mb-4 flex items-center gap-2 text-[#78350F] ps-2">
-           <RiShoppingBag3Line className="w-6 h-6" />
-           <span>Artisanat à proximité</span>
-         </h3>
-            {nearbyArtisansWithDetails === undefined ? (
-             <ArtisanGridSkeleton count={6} />
-           ) : nearbyArtisansWithDetails && nearbyArtisansWithDetails.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              {nearbyArtisansWithDetails.slice(0, 6).map((item) => (
-                <ArtisanCard
-                  key={item.artisan._id}
-                  name={item.artisan.name}
-                  location={`${item.artisan.location.city}, ${item.artisan.location.department} (${item.distance.toFixed(1)} km)`}
-                  categories={item.artisan.categories}
-                  image={item.artisan.images[0] || "/images/artisanal-shop-img/weaver-shop.svg"}
-                />
-              ))}
-            </div>
-          ) : (
-            <p className="text-gray-600 text-center">Aucun artisan trouvé à proximité</p>
-          )}
-       </div>
+      {/* Composant Artisans */}
+      <div className="max-w-3xl mx-auto pb-5 ">
+        <h3 className="text-base md:text-lg font-semibold mb-4 flex items-center gap-2 text-[#78350F] ps-2">
+          <RiShoppingBag3Line className="w-6 h-6" />
+          <span>Artisanat à proximité</span>
+        </h3>
+        {nearbyArtisansWithDetails === undefined ? (
+          <ArtisanGridSkeleton count={6} />
+        ) : nearbyArtisansWithDetails &&
+          nearbyArtisansWithDetails.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {nearbyArtisansWithDetails.slice(0, 6).map((item) => (
+              <ArtisanCard
+                key={item.artisan._id}
+                id={item.artisan._id}
+                name={item.artisan.name}
+                location={`${item.artisan.location.city}, ${item.artisan.location.department} (${item.distance.toFixed(1)} m)`}
+                categories={item.artisan.categories}
+                image={
+                  item.artisan.images[0] ||
+                  "/images/artisanal-shop-img/weaver-shop.svg"
+                }
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-gray-600 text-center">
+            Aucun artisan trouvé à proximité
+          </p>
+        )}
+      </div>
     </div>
   );
 }
